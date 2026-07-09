c904501db95db4a80729f29f77fc154484af4627
HazelDaniel <hazeldaniel856@gmail.com>
2026-07-09 12:53:37 +0100

    feat: expose package definition file

--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,22 @@ export default defineConfig({
     babel({
       presets: [reactCompilerPreset()],
     }),
-    dts(),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.app.json"),
+      rollupTypes: true,
+      insertTypesEntry: true,
+      compilerOptions: {
+        declaration: true,
+        emitDeclarationOnly: true,
+        allowImportingTsExtensions: false,
+      },
+    }),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "GraphGiraffeReact",
-      formats: ["es"],
+      formats: ["es", "cjs"],
       fileName: "index",
     },
     rollupOptions: {
